790a0a01717b95c9673288ef417e2152378f78f2
Adham <87741709+AdhamChan@users.noreply.github.com>
2025-01-24 06:03:18 +0800

    added background img

--- iris-MLPROJECT.py
+++ iris-MLPROJECT.py
@@ -9,6 +9,15 @@ model = joblib.load('gradient_boosting.pkl')
 st.markdown(
     """
     <style>
+
+     body {
+        background-image: url("https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: center;
+    }
+
     .title {
         font-size: 36px;
         font-weight: bold;
